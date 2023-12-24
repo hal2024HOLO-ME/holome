8223416c0a159e186bdf5f48f60b7feaa1af3282
Nichola03 <fkdream03@gmail.com>
2023-12-25 02:27:53 +0900

    fix: なつき度１だけあげてる

--- Assets/HOLOMEProject/Script/CollisionDetection/ShowerCollisionDetection.cs
+++ Assets/HOLOMEProject/Script/CollisionDetection/ShowerCollisionDetection.cs
@@ -8,7 +8,6 @@ public class ShowerCollisionDetection : MonoBehaviour
     /// シャワーが使用できるかどうかを表すフラグ。
     /// </summary>
     public static bool isShowerUsed = true;
-    public bool isCollisionAndshowerUsed = isShowerUsed;
     private CharacterModel characterModel;
 
     public void SetCharacterModel(CharacterModel characterModel)
@@ -28,8 +27,8 @@ public class ShowerCollisionDetection : MonoBehaviour
     /// <returns></returns>
     private IEnumerator HandleCollision(GameObject collision)
     {
-        isShowerUsed = true;
-        if (isCollisionAndshowerUsed || characterModel.GetIsDead())
+        bool isCollisionAndshowerUsed = isShowerUsed || characterModel.GetIsDead();
+        if (isCollisionAndshowerUsed)
         {
             Animator animator = characterModel.GetGameObject().GetComponent<Animator>();
             animator.SetTrigger("HappyTrigger");
@@ -40,7 +39,7 @@ public class ShowerCollisionDetection : MonoBehaviour
             {
                 nostalgicManager.ChangeObjectSize();
             }
-             isShowerUsed = false;
+            isShowerUsed = false;
         }
         yield return null;
     }
